4529df54413efe553865fef4f6519ad2b0dbb893
stephin007 <stephinreji123@gmail.com>
2020-10-13 16:30:14 +0530

    Added Browser router over all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import axios from 'axios'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 import Navbar from './components/layout/Navbar'
 import Alert from './components/layout/Alert'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
+import About from './components/pages/About'
 
 import './App.css';
 
@@ -52,20 +54,29 @@ class App extends Component {
   
   render(){
     return (
-      <div >
-        <Navbar title="Github Finder" icon="fab fa-github"/>
-        <div className="container">
-          <Alert alert={this.state.alert}/>
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert} />
-        <Users 
-          loading={this.state.loading} 
-          users={this.state.users} />
+      <Router>
+        <div >
+          <Navbar title="Github Finder" icon="fab fa-github"/>
+          <div className="container">
+            <Alert alert={this.state.alert}/>
+            <Switch>
+              <Route exact path='/' render={props =>(
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={this.state.users.length > 0 ? true : false}
+                    setAlert={this.setAlert} />
+                  <Users 
+                  loading={this.state.loading} 
+                  users={this.state.users} />
+                </Fragment>
+              )} />
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </ Router>  
     );
   }
 }
