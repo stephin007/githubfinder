de4eb16e756e865d45993444df04f56341e96e46
stephin007 <stephinreji123@gmail.com>
2020-10-19 13:37:08 +0530

    Added the not found component in the App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Alert from './components/layout/Alert'
 import User from './components/users/User'
 import About from './components/pages/About'
 import Home from './components/pages/Home'
+import NotFound from './components/pages/Notfound'
 
 // import GithubContext from './context/github/githubContext'
 import  GithubState from './context/github/GithubState'
@@ -33,6 +34,7 @@ const App = ()=> {
                   <Route exact path='/' component={Home}/>
                   <Route exact path='/about' component={About}/>
                   <Route exact path='/user/:login' component= {User}/>
+                  <Route component= {NotFound}/>
                 </Switch>
               </div>
             </div>
